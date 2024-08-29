336d299e131b14bbad113567a9e44f912daacefc
sanjay <sanjayvkumar787@gamil.com>
2024-08-29 17:14:23 +0530

    first commit

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,11 @@
+let baseUrl = "https://fakestoreapi.com";
+
+async function fetchFromApi(path) {
+  const res = await fetch(`${baseUrl}/${path}`);
+  const data = await res.json();
+  return data;
+}
+
+export default fetchFromApi;
+
+
